91a78df8c4fef9f839528fee1e227f744dc46584
danchamorro <dan@danchamorro.com>
2019-01-14 16:04:27 -0500

    Toggling Todos / "TOGGLE_TODO" case

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import TodosContext from "../context";
 
 export default function TodoList() {
-  const { state } = useContext(TodosContext);
+  const { state, dispatch } = useContext(TodosContext);
 
   const title =
     state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing To Do!";
@@ -16,7 +16,15 @@ export default function TodoList() {
             key={todo.id}
             className="bg-orange-dark border-black border-dashed border-2 my-2 py4 flex items-center"
           >
-            <span className="curser-pointer flex-1 ml-12">{todo.text}</span>
+            <span
+              onDoubleClick={() =>
+                dispatch({ type: "TOGGLE_TODO", payload: todo })
+              }
+              className={`curser-pointer flex-1 ml-12 ${todo.complete &&
+                "line-through text-grey-darkest"}`}
+            >
+              {todo.text}
+            </span>
             <button>
               <img
                 src="https://icon.now.sh/edit/0050c5"
